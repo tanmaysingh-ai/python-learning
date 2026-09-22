91c6856105472ee0ff5c262b65cc8f678e4151ce
Tanmay Singh <tanmaysingh2439525@gmail.com>
2026-09-23 01:01:44 +0530

    update first python program

--- code1.py
+++ code1.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("Hello, Github!")
+name = "Tanmay Singh"
+age = 19 
+university = "BBD UNIVERSITY"
+course = "B.tech CSE AI"
+print("Name:",name) 
+print("Age",age)
+print("University",university)
+print("Course",course)
+
